04d8608371430d1ebe871db1fd4e3f5b1e59cfc9
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2014-05-04 22:07:29 +0100

    adding first version of the library and initial import into github

---
+++ README.rst
@@ -0,0 +1,84 @@
+Photoset Gallery
+================
+
+.. contents:
+
+Introduction
+------------
+
+Generates a gallery from a photoset on Flickr with links referring to the
+originals. This provides a JQuery plugin which uses the excellent JCarousel
+to manage the gallery side of things.
+
+Useage
+------
+
+Clone the repository and open demo-gallery.html in a browser. You will need to
+provide your Flickr API key. Paste it into the input and press Display and you
+should see:
+
+.. image:: working-control.jpg
+    :width: 50%
+    :align: center
+
+The HTML contains the following data-photoset attribute which tells the library
+what photoset to use. In this case this is my Channel Islands Flickr photoset
+https://www.flickr.com/photos/oisinmulvihill/sets/72157606132043161/
+
+.. code-block:: html
+
+    <div class="photoset-gallery" data-photoset="72157606132043161"></div>
+
+The following snippet of code sets up the library to look for all elements with
+the class "photoset-gallery". I don't yet support more then one on a page
+
+.. code-block:: javascript
+
+    $.photoset.gallery.init({
+        api_key: api_key
+    });
+
+Dependancies
+------------
+
+In the demo I was using JQuery 2.1.0 and Carousel 0.3.1 over CDN:
+
+.. code-block:: html
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jcarousel/0.3.1/jquery.jcarousel.min.js"></script>
+
+JCarousel
+~~~~~~~~~
+
+ * http://sorgalla.com/jcarousel/
+
+
+License
+-------
+
+Copyright (c) 2014, Oisin Mulvihill
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of Oisin Mulvihill nor the names of its contributors may
+   be used to endorse or promote products derived from this software without
+   specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ js/jquery.photoset.gallery.js
@@ -0,0 +1,174 @@
+/*jslint devel: true, maxerr: 100, browser: true, indent: 4 */
+/*
+*/
+;(function ($) {
+
+    var settings = {
+        flickr_api: "https://api.flickr.com/services/rest",
+        api_key: ""
+    };
+
+    function template_carousel() {
+        var t = "";
+        t += '<div class="jcarousel-wrapper">';
+        t += '<div class="jcarousel-title"></div>';
+        t += '<div class="jcarousel">';
+        t += '    <div class="loading">Loading carousel items...</div>';
+        t += '</div>';
+        t += '<a href="#" class="jcarousel-control-prev">&lsaquo;</a>';
+        t += '<a href="#" class="jcarousel-control-next">&rsaquo;</a>';
+        t += '<a id="jcarousel-source" title="" href="#"><img id="jcarousel-selected-image" src=""/></a>';
+        t += '</div>';
+        return t;
+    }
+
+    function render_gallery(el, photoset) {
+        console.log(photoset);
+
+        var title = photoset.title;
+        console.log("title: "+ title);
+
+        var owner = photoset.owner;
+        console.log("owner: "+ owner);
+
+        var pictures = [];
+
+        $.each(photoset.photo, function (index, p) {
+            // {
+            //     "farm": 4,
+            //     "id": "2308844218",
+            //     "isprimary": "1",
+            //     "secret": "a60740dfa4",
+            //     "server": "3226",
+            //     "title": "Kitty behind bars"
+            // }
+            // http://farm{id}.staticflickr.com/{server}/{id}_{secret}_[mstzb].jpg
+            var base = "http://farm" + p.farm + ".staticflickr.com/" + p.server + "/" + p.id + "_" + p.secret;
+
+            pictures.push({
+                thumbnail: base + "_m.jpg",
+                picture: base + "_z.jpg",
+                title: p.title || "",
+                src: "http://www.flickr.com/photos/" + owner + "/" + p.id
+            });
+        });
+
+        console.log("pictures: ");
+        console.log(pictures);
+
+        $(el).html(template_carousel());
+
+        var jcarousel = $('.jcarousel').jcarousel();
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+
+        var html = '<ul>';
+
+        $.each(pictures, function() {
+            html += '<li><img class="select-large" data-large="' + this.picture + '" src="' + this.thumbnail + '" alt="' + this.title + '"></a></li>';
+        });
+
+        html += '</ul>';
+
+        // Append items
+        jcarousel.html(html);
+
+        // Reload carousel
+        jcarousel.jcarousel('reload');
+
+        // Set the initially selected image from the set:
+        $("#jcarousel-selected-image").attr("src", pictures[0].picture);
+        $("#jcarousel-selected-image").attr("src", pictures[0].picture);
+
+        $("#jcarousel-source").attr("title", pictures[0].title);
+        $("#jcarousel-source").attr("href", pictures[0].src);
+
+        $(".jcarousel-title").html(title);
+
+        // Set up the click to change select image:
+        $(".select-large").each(function (index, ele){
+            $(ele).click(function (){
+                var large = $(ele).data("large");
+                //console.log("change pic to: " + large);
+                $("#jcarousel-selected-image").attr("src", large);
+                $("#jcarousel-source").attr("title", pictures[0].title);
+                $("#jcarousel-source").attr("href", pictures[0].src);
+            });
+        });
+    }
+
+    function display_gallery_for(el, photosetid) {
+        var request = $.ajax({
+            url: settings.flickr_api,
+            data: {
+                method: "flickr.photosets.getPhotos",
+                api_key: settings.api_key,
+                photoset_id: photosetid,
+                format: "json",
+                nojsoncallback: 1
+            }
+        });
+
+        request.done(function (response) {
+            if (response.hasOwnProperty("photoset")) {
+                render_gallery(el, response.photoset);
+            } else {
+                console.log("no photoset found in response!");
+            }
+        });
+
+        request.fail(function( jqXHR, textStatus ) {
+            alert( "Request failed: " + textStatus );
+        });
+    }
+
+    function init(options) {
+        if (options) {
+            $.extend(settings, options);
+        }
+
+        $(".photoset-gallery").each(function (index, el){
+            var photosetid = $(el).data("photoset");
+            if (!photosetid) {
+                console.log("missing data-photoset found on: "+ el);
+                return;
+            }
+
+            console.log("photoset to recover: "+photosetid);
+            display_gallery_for($(el), photosetid);
+        });
+
+    }
+
+    // Add to the JQuery plugin namespace i.e. $.photoset.<XYZ calls>
+    // create $.photoset if its not present.
+    if (!$.photoset) { $.extend({photoset: {}}); }
+
+    // Now add $.bookingsys.profile namespace contents:
+    $.extend($.photoset, {gallery: {
+        init: init
+    }});
+
+})(jQuery);
+
